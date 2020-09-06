c256679704451bc326f377d74df65cf538e1624f
Alexey Knyazev <aleks.kn@gmail.com>
2020-09-06 20:50:24 +0300

    fix

--- src/main/java/com/dappcraft/WSServer.java
+++ src/main/java/com/dappcraft/WSServer.java
@@ -67,18 +67,18 @@ public class WSServer {
         if(type.equals("scene")) {
             LOG.infov("onMessage scene socket {0}: {1}", user, message);
         } else if (type.equals("controller")) {
-            WsMessage msg = parse(message);
-            LOG.infov("controller msg {0}, {1}", user, msg.getType());
+//            WsMessage msg = parse(message);
+//            LOG.infov("controller msg {0}, {1}", user, msg.getType());
             if (sceneSessions.containsKey((user))) {
-                if (msg.getType().equals("rotate")) {
-                    LOG.infov("send {0}, {1}, {2}, {3}", msg.getQuat()[0], msg.getQuat()[1], msg.getQuat()[2], msg.getQuat()[3]);
-                    message = gson.toJson(msg.getQuat());
+//                if (msg.getType().equals("rotate")) {
+//                    LOG.infov("send {0}, {1}, {2}, {3}", msg.getQuat()[0], msg.getQuat()[1], msg.getQuat()[2], msg.getQuat()[3]);
+//                }
+//                    message = gson.toJson(msg.getQuat());
                     sceneSessions.get(user).getAsyncRemote().sendObject(message, result -> {
                         if (result.getException() != null) {
                             LOG.errorv(result.getException(), "Unable to send message to scene for {0}", user);
                         }
                     });
-                }
             }
         } else {
             LOG.errorv("onMessage error, unknown type {0}, {1}", type, user);
